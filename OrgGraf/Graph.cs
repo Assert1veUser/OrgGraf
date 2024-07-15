44c972783724ccd15f8f3f227e10b16e606a8a8c
Assert1veUser <tony.markin2017@yandex.ru>
2024-07-15 15:19:43 +0300

    Changes to be committed: 	new file:   .idea/.idea.OrgGraf/.idea/.gitignore 	new file:   .idea/.idea.OrgGraf/.idea/encodings.xml 	new file:   .idea/.idea.OrgGraf/.idea/indexLayout.xml 	new file:   OrgGraf.sln 	new file:   OrgGraf/Edge.cs 	new file:   OrgGraf/Graph.cs 	new file:   OrgGraf/OrgGraf.csproj 	new file:   OrgGraf/Program.cs 	new file:   OrgGraf/Vertex.cs 	new file:   OrgGraf/obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   OrgGraf/obj/Debug/net7.0/OrgGraf.AssemblyInfo.cs 	new file:   OrgGraf/obj/Debug/net7.0/OrgGraf.AssemblyInfoInputs.cache 	new file:   OrgGraf/obj/Debug/net7.0/OrgGraf.GeneratedMSBuildEditorConfig.editorconfig 	new file:   OrgGraf/obj/Debug/net7.0/OrgGraf.GlobalUsings.g.cs 	new file:   OrgGraf/obj/Debug/net7.0/OrgGraf.assets.cache 	new file:   OrgGraf/obj/Debug/net7.0/OrgGraf.csproj.AssemblyReference.cache 	new file:   OrgGraf/obj/OrgGraf.csproj.nuget.dgspec.json 	new file:   OrgGraf/obj/OrgGraf.csproj.nuget.g.props 	new file:   OrgGraf/obj/OrgGraf.csproj.nuget.g.targets 	new file:   OrgGraf/obj/project.assets.json 	new file:   OrgGraf/obj/project.nuget.cache 	new file:   OrgGraf/obj/project.packagespec.json 	new file:   OrgGraf/obj/rider.project.restore.info

---
+++ OrgGraf/Graph.cs
@@ -0,0 +1,32 @@
+﻿namespace OrgGraf;
+
+public class Graph
+{
+    List<Vertex> V = new List<Vertex>();
+    List<Edge> E = new List<Edge>();
+    private int[,] matrix;
+    public int VertexCount => V.Count;
+    public int EdgeCount => E.Count;
+    public void AddVertex(Vertex vertex)
+    {
+        V.Add(vertex);
+    }
+    public void AddEdge(Vertex from, Vertex to)
+    {
+        var edge = new Edge(from, to);
+        E.Add(edge);
+    }
+
+    public List<Vertex> GetVertexLists(Vertex vertex)
+    {
+        var result = new List<Vertex>();
+        foreach (var egde in E)
+        {
+            if (egde.From == vertex)
+            {
+                result.Add(egde.To);
+            }
+        }
+        return result;
+    }
+}
